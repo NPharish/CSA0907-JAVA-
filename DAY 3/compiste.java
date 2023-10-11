41f08b09c51784dc5ed2cb46ba6d384e473fde62
NPharish <112629810+NPharish@users.noreply.github.com>
2023-10-11 14:13:43 +0530

    Add files via upload

---
+++ DAY 3/compiste.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class compiste
+ {
+public static void main(String[] args) 
+{
+Scanner input=new Scanner(System.in);
+int size = input.nextInt();
+int []arr = new int	[size];
+int len=arr.length;
+int count=0;
+for(int i=0;i<len;i++)
+{
+ int c=0;
+ for(int j=1;j<100;j++)
+ {
+ if(arr[i]%j==0)
+ {
+ c++;
+ }
+ }
+ if(c>2)
+ count++;
+}
+System.out.println(count);
+}
+}
+
---
+++ DAY 3/squareroot.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class square root
+ {
+Scanner input=new Scanner(System.in);
+float n=input.nextFloat();
+System.out.print("square: "+(n*n));
+System.out.print("cube: "+(n*n*n));
+ for(int j=1;j<100;j++)
+ {
+ if(arr[i]%j==0)
+ {
+ c++;
+ }
+ }
+ if(c>2)
+ count++;
+}
+System.out.println(count);
+}
+}
+
